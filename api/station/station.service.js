c3040edb58c3237038b1bcb7d80825990c5b87c9
islam abo mokh <alo.jalal.55@gmail.com>
2023-07-03 19:05:41 +0300

    bug in song id when add song fixed

--- api/station/station.service.js
+++ api/station/station.service.js
@@ -5,7 +5,7 @@ const ObjectId = require('mongodb').ObjectId
 async function query() {
   try {
     const collection = await dbService.getCollection('playlist')
-    console.log('collection', collection)
+
     var stations = await collection.find().toArray()
     console.log('stations in service back', stations)
     return stations
@@ -73,27 +73,49 @@ async function update(station) {
 async function addStationSong(stationId, song) {
   try {
     const collection = await dbService.getCollection('playlist')
-    const songWithId = { ...song, _id: new ObjectId() }
+    console.log('song before songWithId ', song)
+    // const songWithId = { ...song, _id: new ObjectId() }
+    console.log('song after songWithId ', song)
     await collection.updateOne(
       { _id: new ObjectId(stationId) },
-      { $push: { songs: songWithId } }
+      { $push: { songs: song } }
     )
-    return songWithId
+    return song
   } catch (err) {
     logger.error(`cannot add station song ${stationId}`, err)
     throw err
   }
 }
 
+// async function removeStationSong(stationId, songId) {
+//   console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
+//   try {
+//     const collection = await dbService.getCollection('playlist')
+//     const station = await collection.findOne({ _id: new ObjectId(stationId) })
+//     console.log('station', station)
+//     const newSongs = station?.songs.filter((song) => song._id !== songId)
+//     console.log('newSongs', newSongs)
+//     await dbService
+//       .getCollection('playlist')
+//       .updateOne(
+//         { _id: new ObjectId(stationId) },
+//         { $set: { songs: newSongs } }
+//       )
+//     return station
+//   } catch (err) {
+//     logger.error(`cannot remove song ${songId} from station ${stationId}`, err)
+//     throw err
+//   }
+// }
 async function removeStationSong(stationId, songId) {
-  console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii')
   try {
     const collection = await dbService.getCollection('playlist')
-    await collection.updateOne(
+    const station = await collection.updateOne(
       { _id: new ObjectId(stationId) },
       { $pull: { songs: { _id: songId } } }
     )
-    return { _id: songId }
+    console.log('station', station)
+    return songId
   } catch (err) {
     logger.error(`cannot add station msg ${stationId}`, err)
     throw err
