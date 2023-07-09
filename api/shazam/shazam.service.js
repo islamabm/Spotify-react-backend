6464c0c189be197bf3db17ca95477ad980a2b4d6
islam abo mokh <alo.jalal.55@gmail.com>
2023-07-09 22:13:42 +0300

    bug in shazam

--- api/shazam/shazam.service.js
+++ api/shazam/shazam.service.js
@@ -1,64 +1,82 @@
 const axios = require('axios')
 const FormData = require('form-data')
-const { Readable } = require('stream')
-const ffmpeg = require('fluent-ffmpeg')
 require('dotenv').config()
 
 async function identifySong(audioData) {
-  // Convert webm to mp3
-  const inputStream = new Readable()
-  inputStream.push(audioData.buffer)
-  inputStream.push(null)
-
-  let mp3Data = []
-
-  const converter = ffmpeg(inputStream)
-    .inputFormat('webm')
-    .outputFormat('mp3')
-    .on('error', console.error)
-    .pipe()
-
-  await new Promise((resolve, reject) => {
-    converter
-      .on('end', () => {
-        mp3Data = Buffer.concat(mp3Data)
-        resolve()
-      })
-      .on('error', reject)
-  })
-
-  // Create a Readable stream from the mp3 Buffer
-  const stream = new Readable()
-  stream.push(mp3Data)
-  stream.push(null)
-
-  // Append the stream to form data
-  const formData = new FormData()
-  formData.append('file', stream, {
-    filename: 'recordedAudio.mp3', // Make sure this is an actual mp3 file
-    contentType: 'audio/mpeg', // Use the correct MIME type for mp3
-  })
-
-  const options = {
-    method: 'POST',
-    url: 'https://music-identify.p.rapidapi.com/identify',
-    headers: {
-      'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-      'X-RapidAPI-Host': 'music-identify.p.rapidapi.com',
-      ...formData.getHeaders(),
-    },
-    data: formData,
-  }
-
   try {
-    const response = await axios.request(options)
-    console.log(
-      'responsesssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
-      response
-    )
-    return response.data
-  } catch (error) {
-    console.error(error)
+    // Convert audioData to base64
+    const audioDataBase64 = Buffer.from(audioData.buffer).toString('base64')
+
+    // Send POST request to Convertio for conversion
+    let response = await axios({
+      method: 'post',
+      url: 'https://api.convertio.co/convert',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        apikey: process.env.CONVERTIO_API_KEY,
+        input: 'base64',
+        file: audioDataBase64,
+        format: 'mp3',
+      },
+    })
+
+    // Save the process ID for polling conversion status
+    let processId = response.data.data.id
+
+    // Poll the API every 5 seconds to check conversion status
+    let timer = setInterval(async () => {
+      let jobResponse = await axios.get(
+        `https://api.convertio.co/convert/${processId}/status`,
+        {
+          params: {
+            apikey: process.env.CONVERTIO_API_KEY,
+          },
+        }
+      )
+
+      // If status is "finished", download the MP3 file
+      if (jobResponse.data.status === 'finished') {
+        let downloadUrl = jobResponse.data.output.url
+        let fileResponse = await axios.get(downloadUrl, {
+          responseType: 'arraybuffer',
+        })
+
+        let mp3Data = new Uint8Array(fileResponse.data)
+        console.log('mp3Dataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', mp3Data)
+        clearInterval(timer)
+
+        // Once mp3 is downloaded, use Shazam API to identify song
+        let songFormData = new FormData()
+        let songBlob = new Blob([mp3Data.buffer], { type: 'audio/mpeg' })
+        songFormData.append('file', songBlob, 'recordedAudio.mp3')
+        console.log(
+          'songFormDataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+          songFormData
+        )
+
+        const options = {
+          method: 'POST',
+          url: 'https://music-identify.p.rapidapi.com/identify',
+          headers: {
+            'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+            'X-RapidAPI-Host': 'music-identify.p.rapidapi.com',
+            ...songFormData.getHeaders(),
+          },
+          data: songFormData,
+        }
+
+        try {
+          const songResponse = await axios.request(options)
+          return songResponse.data
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    }, 5000)
+  } catch (err) {
+    console.error(err)
   }
 }
 
